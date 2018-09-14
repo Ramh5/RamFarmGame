ac85f4d0b8f55c93bc763c73f3368c2381da97d0
Ram Homier <ram.homier@gmail.com>
2018-09-13 21:32:48 -0400

    yield logic

--- farmGame/src/homier/farmGame/model/FarmPlot.java
+++ farmGame/src/homier/farmGame/model/FarmPlot.java
@@ -3,46 +3,57 @@ package homier.farmGame.model;
 
 
 
+import java.util.TreeMap;
+
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
+
 
 public class FarmPlot extends Tile {
 	private double growthRate;
 	private DoubleProperty growth;
-	private IntegerProperty yield ;
+	private IntegerProperty yield;
+	private int maxYield;
+	private TreeMap<Integer, Integer> yieldMap;
 	private String product;
 	private int quality;
 	
-	public FarmPlot(String ID, double growthRate, int yield) {
+	
+	public FarmPlot(String ID, double growthRate, int maxYield) {
 		super(ID);
 		this.growthRate = growthRate;
 		this.growth = new SimpleDoubleProperty(0);
-		this.yield = new SimpleIntegerProperty(yield);
+		this.yield= new SimpleIntegerProperty(0);
+		this.maxYield = maxYield;
+		yieldMap = buildYieldMap();
 		this.product = ID.substring(0, ID.indexOf("_"));
 		this.quality = 0;
 	
 		
 	}
 	
-	public FarmPlot(String ID, double growthRate, double growth, int yield, int quality) {
+	public FarmPlot(String ID, double growthRate, double growth, int maxYield, int quality) {
 		super(ID);
 		this.growthRate = growthRate;
 		this.growth = new SimpleDoubleProperty(growth);
-		this.yield = new SimpleIntegerProperty(yield);
+		this.yield= new SimpleIntegerProperty(0);
+		this.maxYield = maxYield;
+		
 		this.product = ID.substring(0, ID.indexOf("_"));
 		this.quality = quality;
 	
 		
 	}
 	
+
+
 	public void update(double dTime){
-		if(growth.get()<100)
+		if(growth.get()<150)
 		growth.set(growth.get()+growthRate*dTime);
+		yield.set(calculateYield());
 	}
 	
 	public double getGrowthRate() {
@@ -89,5 +100,41 @@ public class FarmPlot extends Tile {
 		return (super.toString() + String.format("\tGrowth Rate: %.0f", growthRate) + String.format("\t  Growth: %.0f", growth) + "\tyield: " + yield + "\tproduct: " + "\tquality: " + quality);
 	}
 	
+	//private helper methode to create the YieldMap
+	private TreeMap<Integer, Integer> buildYieldMap() {
+		TreeMap<Integer, Integer> treeMap = new TreeMap<Integer,Integer>();
+		treeMap.put(0,0);
+		treeMap.put(50,0);
+		treeMap.put(80,30);
+		treeMap.put(95,98);
+		treeMap.put(100,100);
+		treeMap.put(110,98);
+		treeMap.put(115,95);
+		treeMap.put(130,70);
+		treeMap.put(150,0);
+		return treeMap;
+	}
 
+	//private helper methode to calculate the yield
+	private int calculateYield(){
+		int growthInt =(int) growth.get();
+		double yield=0;
+		float yieldFactor;
+
+		int x1 =  yieldMap.floorKey(growthInt);
+		int x2 =  yieldMap.ceilingKey(growthInt);
+		float y1 =  yieldMap.get(x1).floatValue();
+		float y2 =  yieldMap.get(x2).floatValue();
+		//to prevent devision by zero
+		if(x2-x1==0){
+			yieldFactor = y1;
+		}else{
+			yieldFactor =  ((y2-y1)/((float)(x2-x1)))*(growthInt-x1)+y1;
+		}
+		
+			yield = (yieldFactor/100*maxYield);
+			
+		return (int)yield;
+	}
+	
 }
